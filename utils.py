f318a847bf6d4d66f6bc36ded2167e9c50b0e347
shadesofkrish <kkrishh812@gmail.com>
2026-09-11 10:06:57 +0530

    new utils.py

--- utils.py
+++ utils.py
@@ -1,36 +1,22 @@
-import numpy as np
+"""
+Utility Functions
+"""
 import matplotlib.pyplot as plt
 
 
-def plot_data(X, y, ax):
-    """
-    Plots the data points for logistic regression.
+def plot_data(X, y, ax, pos_label="y=1", neg_label="y=0", s=80, loc='best' ):
+    """ plots logistic data with two axis """
+    # Find Indices of Positive and Negative Examples
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  #work with 1D or 1D y vectors
+    neg = neg.reshape(-1,)
 
-    Args:
-        X (ndarray): Input features with shape (m, 2)
-        y (ndarray): Target values with shape (m, 1)
-        ax: Matplotlib axis
-    """
+    # Plot examples
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c = 'green', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='.', s=s, label=neg_label, facecolors='none', edgecolors='#0096ff', lw=3)
+    ax.legend(loc=loc)
 
-    # Plot positive examples
-    pos = y.flatten() == 1
-    ax.scatter(
-        X[pos, 0],
-        X[pos, 1],
-        marker='x',
-        s=80,
-        linewidth=2,
-        label='y = 1'
-    )
-
-    # Plot negative examples
-    neg = y.flatten() == 0
-    ax.scatter(
-        X[neg, 0],
-        X[neg, 1],
-        marker='o',
-        s=80,
-        label='y = 0'
-    )
-
-    ax.legend()
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
